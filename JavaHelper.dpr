e2da7f61a1490687f06f5ad6711c4029c2ae60df
HuachunLuo <ehuae@163.com>
2017-09-12 16:49:10 +0800

    应用程序
    更新应用程序名称

---
+++ JavaHelper.dpr
@@ -0,0 +1,15 @@
+program JavaHelper;
+
+uses
+  Forms,
+  uMainForm in 'uMainForm.pas' {Form1},
+  uConst in 'uConst.pas',
+  uEnvironmentMgr in 'uEnvironmentMgr.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
